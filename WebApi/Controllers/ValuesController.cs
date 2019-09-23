27bb28a502629c6248e5a635f04f0fc38e78084e
DESKTOP-GT4AAMB\stars <apollo.estrella@gmail.com>
2019-09-23 09:46:45 +0800

    Web Api Initial commit

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            List<Author> authors = new List<Author>();
+            authors.Add(new Author(1, "Adam", "West"));
+            authors.Add(new Author(2, "Tony", "Parker"));
+            var x = new JsonResult(authors);
+            return new JsonResult(authors);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody] Author author)
+        {
+            return Ok();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Author author)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
